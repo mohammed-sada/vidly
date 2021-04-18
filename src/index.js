4e998be7013da2a113eae40f76fc60df268d7177
mohammed-sada <mohammedabusada94@example.com>
2021-04-18 13:50:35 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import { BrowserRouter } from "react-router-dom";
 
-console.log("SUPERMAN", process.env.REACT_APP_NAME);
+console.log("SUPERMAN", process.env.REACT_APP_API_URL);
 
 ReactDOM.render(
   <BrowserRouter>
